1f35ae6dcecbdd6486416f5b93871a511dd32a01
Thisandu-Gunathilake <thisandugunathilake12345@gmail.com>
2026-03-22 14:42:09 +0530

    importing navbar to home page

--- ceylonsync/app/page.tsx
+++ ceylonsync/app/page.tsx
@@ -1,7 +1,14 @@
+import Navbar from "../components/Navbar";
+
 export default function Home() {
   return (
     // 1. Combined background gradient directly into the main tag
     <main className="relative min-h-screen flex items-center bg-gradient-to-br from-[#1e3a4a] via-[#0f2027] to-[#203a43] overflow-hidden">
+      
+      {/*Importing navbar*/}
+      <Navbar />
+
+      
     </main>
   );
 }
