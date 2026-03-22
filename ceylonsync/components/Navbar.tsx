b721dac19d0c9693001a6491af42318329066ff2
Thisandu-Gunathilake <thisandugunathilake12345@gmail.com>
2026-03-22 14:33:19 +0530

    Adding logo to navbar

--- ceylonsync/components/Navbar.tsx
+++ ceylonsync/components/Navbar.tsx
@@ -1,4 +1,13 @@
 "use client";
-import React from 'react';
+import React from "react";
 
-export default function Navbar() {}
+export default function Navbar() {
+  return (
+    <nav className="fixed top-8 left-0 w-full z-50 px-6 lg:px-12 flex justify-between items-center">
+      {/* Logo */}
+      <div className="text-white text-2xl font-semibold flex items-center gap-2">
+        CeylonSync
+      </div>
+    </nav>
+  );
+}
