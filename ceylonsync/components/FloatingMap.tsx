5bd2d0c2290a77636451a10d2bdd88af42ff2309
Thisandu-Gunathilake <thisandugunathilake12345@gmail.com>
2026-03-22 23:41:28 +0530

    left aligning the floating map

--- ceylonsync/components/FloatingMap.tsx
+++ ceylonsync/components/FloatingMap.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 
 export default function FloatingMap() {
   return (
-    <div className="relative flex items-center justify-center w-full h-full">
+    <div className="relative flex items-center justify-center w-full h-full overflow-visible">
       {/* Sri Lankan Map */}
       <motion.div
-        animate={{ y: [0, -15, 0] }}
+        animate={{ x: [-10, 10, -10] }}
         transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}
-        className="relative z-10 w-full flex justify-center"
+        className="relative z-10 w-full flex items-center justify-center"
       >
         <Image
           src="/images/map.png"
@@ -17,7 +17,7 @@ export default function FloatingMap() {
           width={800}
           height={800}
           priority
-          className="w-full lg:w-[120%] h-auto max-w-[800px] scale-110 lg:scale-125 drop-shadow-[0_20px_50px_rgba(0,0,0,0.5)] object-contain"
+          className="w-[120%] mx-auto -translate-x-20 lg:translate-x-0 lg:w-[120%] h-auto max-w-[600px] lg:max-w-[800px] scale-125 drop-shadow-[0_20px_50px_rgba(0,0,0,0.5)] object-contain"
         />
       </motion.div>
     </div>
